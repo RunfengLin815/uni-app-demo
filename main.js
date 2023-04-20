9742726d382ec878ff44700fa4433d2a11012f17
RunfengLin815 <1661442416@qq.com>
2023-04-20 23:26:48 +0800

    轮播图

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
@@ -8,18 +7,42 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
+
+
+import {
+	$http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,15 @@
 <template>
 	<view>
-		
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<!-- 循环渲染轮播图的 item 项 -->
+			<swiper-item v-for="(item, i) in swiperList" :key="i">
+				<view class="swiper-item">
+					<!-- 动态绑定图片的 src 属性 -->
+					<image :src="item.image_src"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +17,49 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图数据列表
+				swiperList: []
 			};
+		},
+		onLoad() {
+			// 调用获取轮播图的自定义方法
+			this.getSwiperList()
+		},
+		methods: {
+			// 获取轮播图数据的方法
+			async getSwiperList() {
+				// check
+				console.log(uni.$http)
+				console.log(this.swiperList)
+				// 请求数据
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// 打印查看数据
+				console.log(res)
+				// 返回失败
+				if (res.meta.status !== 200) {
+					return uni.showToast({
+						title: "数据请求失败",
+						duration: 1500, // 1.5秒后自动消失
+						icon: 'none'
+					})
+				}
+				// 挂载数据到data中的列表
+				this.swiperList = res.message
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
 </style>
